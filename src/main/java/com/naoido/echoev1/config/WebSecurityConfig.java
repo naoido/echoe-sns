10f403c76f95c34de213118fb21727546df5cf56
なおいど <sentyouido@gmail.com>
2024-01-27 07:54:34 +0900

    CSRFトークンがないと403になるのでCSRFを無効化

--- src/main/java/com/naoido/echoev1/config/WebSecurityConfig.java
+++ src/main/java/com/naoido/echoev1/config/WebSecurityConfig.java
@@ -1,12 +1,10 @@
 package com.naoido.echoev1.config;
 
-import com.google.firebase.auth.FirebaseToken;
 import com.naoido.echoev1.entity.FirebaseAuthenticationToken;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -17,9 +15,9 @@ public class WebSecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/api/**"))
                 .addFilterBefore(new FirebaseAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/api/**").authenticated());
+                .authorizeHttpRequests(authz -> authz.requestMatchers("/api/**").authenticated());
 
         return http.build();
     }
